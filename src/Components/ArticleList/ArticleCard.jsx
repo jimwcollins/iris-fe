a8307390cc778d41caa270f2b9fb63b921af40ab
James Collins <jimwcollins@gmail.com>
2020-12-08 17:42:39 +0000

    Adds extra info and styling to article list

--- src/Components/ArticleList/ArticleCard.jsx
+++ src/Components/ArticleList/ArticleCard.jsx
@@ -3,12 +3,26 @@ import { Link } from '@reach/router';
 
 const ArticleCard = ({ article }) => {
   return (
-    <li className="articles__card">
-      <Link to={`/article/${article.article_id}`} className="articles__link">
-        <h3>{article.title}</h3>
-        <p>{article.body}</p>
-      </Link>
-    </li>
+    <Link to={`/article/${article.article_id}`} className="articles__link">
+      <li className="articles__card">
+        <h3 className="articles__card__title">{article.title}</h3>
+        <div className="articles__card__main">
+          <p className="articles__card__body">{article.body}</p>
+        </div>
+        <div className="articles__card__info">
+          <p className="articles__card__author">
+            {article.author} •{' '}
+            <span className="articles__card__time">{article.created_at}</span>
+          </p>
+          <div className="articles__card__stats">
+            <p className="articles__card__stat">{article.votes} votes</p>
+            <p className="articles__card__stat">
+              {article.comment_count} comments
+            </p>
+          </div>
+        </div>
+      </li>
+    </Link>
   );
 };
 

80cb3371b3bc5de79e5f2bbf920378c27379866d
James Collins <jimwcollins@gmail.com>
2020-12-11 13:23:28 +0000

    adds create post button

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -21,7 +21,13 @@ class Nav extends Component {
         <div className="nav__bar grid">
           <div className="nav__bar__controls">
             <div className="nav__bar__topic">
-              <button className="btn" onClick={this.showTopics}>
+              <button className="btn btn__space-right">
+                <span className="btn__pencil">&#9998;</span>
+              </button>
+              <button
+                className="btn btn__space-right"
+                onClick={this.showTopics}
+              >
                 Topics
               </button>
               <form>

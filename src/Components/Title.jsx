ee70a20179e57ada5c32745ecac7e701957194d1
James Collins <jimwcollins@gmail.com>
2021-01-19 08:59:37 +0000

    logo and colour changes

--- src/Components/Title.jsx
+++ src/Components/Title.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import { Link } from '@reach/router';
-import logo from '../images/EyeLogo.jpg';
+import logo from '../images/IrisLogo.png';
 
 const Header = () => {
   return (
     <Link to="/" className="title-link">
-      <h1 className="title__text">The</h1>
       <img src={logo} className="logo" alt="iris logo" />
-      <h1 className="title__text">Iris</h1>
     </Link>
   );
 };
